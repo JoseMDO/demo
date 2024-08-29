aeeb2b2680118552f13c91667086fbe744c3c528
JoseMDO <joseamontesdeocamo@lewisu.edu>
2024-08-29 13:14:20 -0500

    added more partials and dynamic pages

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-require "index.view.php";
+$heading = "Home";
+
+
+function dd( $value ) {
+    echo "<pre>";
+    var_dump( $value );
+    echo "</pre>";
+
+    die();
+}
+
+dd( $_SERVER );
+
+require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-
-<head>
-  <meta charset="UTF-8">
-  <title>PHP Test Site</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-  <div class="min-h-full">
-    <nav class="bg-gray-800">
+<nav class="bg-gray-800">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
@@ -20,7 +9,8 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                <a href="/
+                " class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                   aria-current="page">Home</a>
                 <a href="/about.php"
                   class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
@@ -152,19 +142,3 @@
         </div>
       </div>
     </nav>
-
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Welcome To Your Application!</h1>
-      </div>
-    </main>
-  </div>
-
-</body>
-
-</html>
