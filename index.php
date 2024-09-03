8737d53d1a518e68cc922b9047152eafe7bab7b6
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-09-03 11:27:46 -0500

    added database class

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<?php 
+<?php
 
 require "functions.php";
+require "Database.php";
 // require "router.php";
 
+$db = new Database();
 
-
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-$pdo = new PDO($dsn, "root");
-
-$statement = $pdo->prepare("select * from posts");
-
-$statement->execute();
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
-    echo "<li>". $post["title"]. "</li>";
+    echo "<li>" . $post["title"] . "</li>";
 }
