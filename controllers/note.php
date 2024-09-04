88d94a450f1b92fa485c7122b896d08925a4e8ba
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-09-04 10:20:27 -0500

    finished note view and routing

---
+++ controllers/note.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+$config = require("config.php");
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+$note = $db->query('select * from notes where id = :id', [':id' => $id])->fetch();
+
+$heading = "Note {$id}";
+
+require "views/note.view.php";
--- views/note.view.php
+++ views/note.view.php
@@ -4,13 +4,8 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <?php foreach ($notes as $note): ?>
-      <li>
-        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-          <?= $note['body'] ?>
-        </a>
-      </li>
-    <?php endforeach ?>
+    <h1> <?= $note['body'] ?> </h1>
+    <a href="/notes" class="underline text-blue-500 mt-6">Back to notes</a>
   </div>
 </main>
 
