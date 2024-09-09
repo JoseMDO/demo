67f472eed152b8bfe9347f2ef75a16ecd83a20b1
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-09-09 11:49:56 -0500

    added validator class and validation

--- controllers/note_create.php
+++ controllers/note_create.php
@@ -1,18 +1,18 @@
 <?php
 
+require "Validator.php";
+
 $heading = "New Note";
-$config = require("config.php");
+$config = require "config.php";
 $db = new Database($config['database']);
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $errors = [];
+    $validator = new Validator();
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required.';
-    }
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body cannot be more than 1000 characters.';
+    if (! $validator->string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1000 characters is required.';
     }
     
     if (empty($errors)) {
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 # This is a demo site to learn PHP
 
 - Led by Jeffery Way from laracats.com
-
-Test 
