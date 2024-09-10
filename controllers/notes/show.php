66dce2a980f5ccf553b17327f5d3ac917dd51bcd
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-09-10 11:36:25 -0500

    finished restructuring code and folders

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,12 +1,13 @@
 <?php
-
-$config = require("config.php");
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 $id = $_GET['id'];
 $note = $db->query('select * from notes where id = :id', [':id' => $id])->findOrFail();
-$heading = "Note {$id}";
 $currentUserID = 1;
 
 authorize($note['user_id'] === $currentUserID);
 
-require "views/notes/show.view.php";
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
