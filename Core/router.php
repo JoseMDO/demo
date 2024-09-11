2e84f605268caa836730731d1eb537f30277417e
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-09-11 10:17:08 -0500

    added namespacing

--- Core/router.php
+++ Core/router.php
@@ -1,10 +1,9 @@
 <?php   
 
-$routes = require("routes.php");
 
 function routeToController($routes, $uri) {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     };
@@ -12,9 +11,11 @@ function routeToController($routes, $uri) {
 
 function abort($code = 404) {
     http_response_code($code);
-    require "views/{$code}.php";
-    die();
+    require base_path("views/{$code}.php");
+     die();
 }
 
+
+$routes = require base_path("routes.php");
 $uri = parse_url($_SERVER["REQUEST_URI"])['path'];
 routeToController($routes, $uri);
